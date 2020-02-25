563da7287cc50210af76084a56b7052c3e25e622
Jamin Thornsberry <jthornsberry@aspirehealthcare.com>
2020-02-25 12:54:36 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Install
+
+### With [Fisher](https://github.com/jorgebucaran/fisher):
+
+```fish
+fisher add jaminthorns/fish-prompt
+```
+
+### With [Oh My Fish](https://github.com/oh-my-fish/oh-my-fish):
+
+```fish
+omf install https://github.com/jaminthorns/fish-prompt
+```
---
+++ functions/fish_prompt.fish
@@ -0,0 +1,62 @@
+function fish_prompt
+    set exit_status $status
+
+    if not set -q -g _prompt_functions_defined
+        set -g _prompt_functions_defined
+
+        function _git_no_lock
+            git --no-optional-locks $argv
+        end
+
+        function _in_repo
+            _git_no_lock rev-parse --git-dir >/dev/null 2>&1
+        end
+
+        function _repo_branch
+            _git_no_lock symbolic-ref --short --quiet HEAD 2>/dev/null
+        end
+
+        function _repo_commit
+            _git_no_lock rev-parse --short HEAD 2>/dev/null
+        end
+
+        function _repo_clean
+            test -z (echo (_git_no_lock status -s --ignore-submodules=dirty 2>/dev/null))
+        end
+    end
+
+    set left_mark "◥"
+    set right_mark "◣"
+
+    set cyan (set_color -o cyan)
+    set yellow (set_color -o yellow)
+    set red (set_color -o red)
+    set green (set_color -o green)
+    set blue (set_color -o blue)
+    set normal (set_color normal)
+
+    if test $exit_status = 0
+        set left_color $green
+    else
+        set left_color $red
+    end
+
+    set current_dir (basename (prompt_pwd))
+    set left "$left_color$left_mark $blue$current_dir"
+
+    if _in_repo
+        set branch (_repo_branch || _repo_commit)
+
+        if _repo_clean
+            set right_color $green
+        else
+            set right_color $yellow
+        end
+
+        set right "$cyan$branch $right_color$right_mark"
+    else
+        set right "$green$right_mark"
+    end
+
+    echo -n "$left $right $normal"
+end
